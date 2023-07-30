41cfccc17aea2a1444e2385c00128472799fe2f1
Himanshu Vishwakarma <ritik6104@gmail.com>
2023-07-31 03:37:18 +0530

    first problem

--- week 2/day 5/Reverse a link list.cpp
+++ week 2/day 5/Reverse a link list.cpp
@@ -1,1 +1,19 @@
-class
+class Solution {
+public:
+ListNode* ans;
+void fun(ListNode* node){
+    if(node == NULL or node -> next == NULL){
+        ans = node;
+        return;
+    }
+    else{
+        fun(node -> next);
+        (node -> next) -> next = node;
+        node -> next = NULL;
+    }
+}
+    ListNode* reverseList(ListNode* head) {
+        fun(head);
+        return ans;
+    }
+};
